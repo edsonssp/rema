1ce6096f6f1fd7c2dc8e5793847ccd43f31d7e38
edsonsspg <edsonsspg@gmail.com>
2026-05-23 17:05:56 -0300

    w

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { getToken } from 'firebase/messaging';
-import { messaging } from './lib/firebase';
-import { motion, AnimatePresence } from 'motion/react';
-import { 
+//import React, { useState, useEffect } from 'react';
+//import { getToken } from 'firebase/messaging';
+//import { messaging } from './lib/firebase';
+//import { motion, AnimatePresence } from 'motion/react';
+//import { 
   IceCream, 
   X, 
   ShoppingBag, 
